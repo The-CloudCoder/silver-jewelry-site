94bd1bd36498006b75997db9b9a22435f3875f35
Rahul <rahulrnair880@gmail.com>
2025-08-17 14:38:41 +0530

    Updated sites

--- src/App.jsx
+++ src/App.jsx
@@ -18,34 +18,43 @@ export default function App() {
 const categories = [...new Set(products.map((p) => p.category))];
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">Swarom Silver</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-               <li className="nav-item"><Link className="nav-link" to="/shop">Shop</Link></li>
-               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="categoryDropdown" role="button" data-bs-toggle="dropdown">
-                  Categories
-                </a>
-                <ul className="dropdown-menu">
-                   {categories.map((cat) => (
-                    <li><Link className="dropdown-item" to={`/category/${cat.toLowerCase()}`} >{cat}</Link></li>
-                     ))}
-                  
-                
-                </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark-nav">
+  <div className="container">
+    {/* Logo + Brand */}
+    <Link className="navbar-brand d-flex align-items-center" to="/">
+      <img
+        src="/images/logo3.png" // Replace with your logo path
+        alt="Swarom Silver Logo"
+        style={{ height: "60px", marginRight: "10px" }}
+      />
+      
+    </Link>
+
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/shop">Shop</Link></li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="categoryDropdown" role="button" data-bs-toggle="dropdown">
+            Categories
+          </a>
+          <ul className="dropdown-menu">
+            {categories.map((cat) => (
+              <li key={cat}>
+                <Link className="dropdown-item" to={`/category/${cat.toLowerCase()}`}>{cat}</Link>
               </li>
-              <li className="nav-item"><Link className="nav-link" to="/cart">Cart ({cart.length})</Link></li>
-              
-            </ul>
-          </div>
-        </div>
-      </nav>
+            ))}
+          </ul>
+        </li>
+        <li className="nav-item"><Link className="nav-link" to="/cart">Cart ({cart.length})</Link></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 
       <Routes>
         <Route path="/" element={<Home addToCart={addToCart} />} />
